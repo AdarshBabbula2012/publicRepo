0da388fa439c8f3d2b187e58f1ec5e9604eb50ff
Adarsh Babbula <123956450+AdarshBabbula2012@users.noreply.github.com>
2025-06-23 12:23:29 +0530

    Update ab_sample.py

--- ab_sample.py
+++ ab_sample.py
@@ -1,51 +1,47 @@
-import pyspark
+import json
 from pyspark.sql import SparkSession
+import snowflake.connector
+import requests
+import logging
+import sys
 
 spark = SparkSession.builder \
-    .enableHiveSupport() \
+    .appName("Snowflake Integration") \
+    .config("spark.jars.packages", "net.snowflake:spark-snowflake_2.12:3.1.1,org.apache.hadoop:hadoop-aws:3.3.4") \
     .getOrCreate()
 
-credential_id = int(spark.conf.get("spark.nabu.credential_id"))
-credential_type_id = int(spark.conf.get("spark.nabu.credential_type_id"))
 
-def getCredentials(credential_id,credential_type_id):
+sf_credential_id = int(spark.conf.get("spark.nabu.sf_credential_id"))
+sf_credential_type_id = int(spark.conf.get("spark.nabu.sf_credential_type_id"))
+aws_credential_id = int(spark.conf.get("spark.nabu.aws_credential_id"))
+aws_credential_type_id = int(spark.conf.get("spark.nabu.aws_credential_type_id"))
+target_path = spark.conf.get("spark.target.path")
+token = spark.conf.get("spark.nabu.token")
+credential_endpoint_url = spark.conf.get("spark.nabu.fireshots_url")
+
+def fetch_credentials(credential_id, credential_type_id, key_map=('username', 'password')):
     try:
-        token = spark.conf.get("spark.nabu.token")
-        credential_endpoint_url = spark.conf.get("spark.nabu.fireshots_url")
-
-        payload = json.dumps({
-            "credential_id": credential_id,
-            "credential_type_id": credential_type_id
-        })
         headers = {
             'Authorization': token,
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
+        payload = json.dumps({
+            "credential_id": credential_id,
+            "credential_type_id": credential_type_id
+        })
 
-        response = requests.request("POST", credential_endpoint_url, headers=headers, data=payload)
+        response = requests.post(credential_endpoint_url, headers=headers, data=payload)
         response_json = response.json()
 
-        if response.status_code != 200 or 'expired' in str(response_json.get("data")):
-            if response.status_code == 401:
-                error_msg = response_json.get("error_msg")
-                raise Exception(
-                    f"Failed to fetch credentials, status code: {response.status_code}, response: {error_msg}")
-            else:
-                data = response_json.get("data")
-                raise Exception(f"Failed to fetch credentials, status code: {response.status_code}, response: {data}")
+        if response.status_code != 200 or 'expired' in str(response_json.get("data", "")):
+            raise Exception(response_json.get("error_msg") or response_json.get("data"))
 
-        elif response.status_code == 200 and 'User does not have ' in str(response_json.get("data")):
-            data = response_json.get("data")
-            raise Exception(f"Failed to fetch credentials, status code: {response.status_code}, response: {data}")
+        data = response_json.get('data', {})
+        return data[key_map[0]], data[key_map[1]]
 
-        credential_details = response_json
-        username = credential_details['data']['username']
-        password = credential_details['data']['password']
-
-        return username,password
-    except (ValueError, Exception) as e:
-        logging.error(f"[+] Exception:  Error has occurred: {e}")
+    except Exception as e:
+        logging.error(f"Failed to fetch credentials: {e}")
         sys.exit(1)
 
-username,password = getCredentials(credential_id,credential_type_id)
+sfUser, sfPassword = fetch_credentials(sf_credential_id, sf_credential_type_id, key_map=('username', 'password'))
